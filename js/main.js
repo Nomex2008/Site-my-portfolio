aea9c3f77ec667cd00bf9683c9d121f7b48a90b6
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-07-07 22:41:02 +0300

    Commit 4
    Add adaptive burger.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-button');
+const navBtnImg = document.querySelector('#nav-button-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = 'img/btn/2.png'
+    } else {
+        navBtnImg.src = 'img/btn/1.png'
+    }
+}
